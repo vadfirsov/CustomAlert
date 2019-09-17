f2797b9e1e909ee4d0337d1466422225b9e49fc4
Vadim Firsov <4firsov@gmail.com>
2019-09-17 11:29:35 +0300

    initial commit

---
+++ CustomAlert/CustomAlertVC.h
@@ -0,0 +1,17 @@
+//
+//  CustomAlertVC.h
+//  CustomAlert
+//
+//  Created by VADIM FIRSOV on 17/09/2019.
+//  Copyright © 2019 VADIM FIRSOV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CustomAlertVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CustomAlert/CustomAlertVC.m
@@ -0,0 +1,32 @@
+//
+//  CustomAlertVC.m
+//  CustomAlert
+//
+//  Created by VADIM FIRSOV on 17/09/2019.
+//  Copyright © 2019 VADIM FIRSOV. All rights reserved.
+//
+
+#import "CustomAlertVC.h"
+
+@interface CustomAlertVC ()
+
+@end
+
+@implementation CustomAlertVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
